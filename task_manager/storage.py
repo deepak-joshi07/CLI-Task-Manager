34c2e3854e844617e0845ec408bf589ff82f078d
Deepak <deepakjoshi98@zohomail.in>
2026-03-25 16:10:38 +0530

    feat: implement storage function

---
+++ task_manager/storage.py
@@ -0,0 +1,15 @@
+import os 
+import json
+
+def load_task(json_file_path):
+    data = {}
+
+    if os.path.exists(json_file_path) and os.path.getsize(json_file_path)>0:
+        with open(json_file_path , mode ='r' , encoding='utf-8') as f:
+            data = json.load(f)
+    return data
+
+def save_task(file_path , task):
+    if os.path.exists(file_path):
+        with open(file_path , mode = 'w' , encoding= 'utf-8') as write_file: 
+            json.dump(task , write_file , indent=4)
