47b8fc9088b1e889e8d5ef80b05325f150430fbf
Deepak <deepakjoshi98@zohomail.in>
2026-03-31 14:57:33 +0530

    feat: add mark complete feature

--- task_manager/__main__.py
+++ task_manager/__main__.py
@@ -1,49 +1,62 @@
 from task_manager.service import TaskManager
 
 manager = TaskManager()
+
+
 def add_task():
     try:
-        task = input('Enter the task you want to add : ')
-        priority = int(input('Enter the priority that you want to add : '))
-        task_added = manager.add_task(task , priority)
+        task = input('Enter the task you want to add: ')
+        priority = int(input('Enter the priority you want to assign: '))
+        task_added = manager.add_task(task, priority)
 
-        print( 
-        f"Task added successfully\n"
-        f"Task ID: {task_added['task_id']}\n"
-        f"Task: {task_added['task']}\n"
-        f"Priority: {task_added['priority']}")
-    except ValueError as e: 
-        print(f"Error : {e}")
+        print(
+            f"\nTask added successfully\n"
+            f"Task ID: {task_added['task_id']}\n"
+            f"Task: {task_added['task']}\n"
+            f"Priority: {task_added['priority']}\n"
+            f"Completed: {task_added['completed']}"
+        )
+    except ValueError as e:
+        print(f"Error: {e}")
 
 
 def list_tasks():
-    tasks = manager.list_task()
+    tasks = manager.list_tasks()
 
-    if not tasks: 
+    if not tasks:
         print("\nNo tasks available.")
-        return 
-    
-    print("\nListing all the added task : ")
+        return
 
-    for sno ,task_id ,  details in tasks:
-        print(f"{sno}. Task_id : {details['task_id']} , Task : {details['task']} , Priority : {details['priority']}")
+    print("\nListing all tasks:\n")
+
+    for sno, task_id, details in tasks:
+        print(
+            f"{sno}. "
+            f"Task ID: {task_id} | "
+            f"Task: {details['task']} | "
+            f"Priority: {details['priority']} | "
+            f"Completed: {details['completed']}"
+        )
+
 
 def delete_task():
     try:
         list_tasks()
-        sno = int(input('\nEnter the serial number of the number of the task to delete: '))
-        delete_task = manager.delete_task(sno)
-        print(f'\nSucessfully deleted the task with id ')
-        print(f"Task : {delete_task['task']}")
-        print(f"Priority : {delete_task['priority']}")
+        sno = int(input('\nEnter the serial number of the task to delete: '))
+        deleted_task = manager.delete_task(sno)
 
-    except ValueError as e : 
-        print(f'Error: {e}')
+        print("\nTask deleted successfully")
+        print(f"Task: {deleted_task['task']}")
+        print(f"Priority: {deleted_task['priority']}")
+        print(f"Completed: {deleted_task['completed']}")
+
+    except ValueError as e:
+        print(f"Error: {e}")
 
 
 def edit_task():
-    try: 
-        list_tasks() 
+    try:
+        list_tasks()
         sno = int(input("\nEnter the serial number of the task to edit: "))
 
         new_task = input("Enter new task (leave blank to keep same): ").strip()
@@ -52,12 +65,28 @@ def edit_task():
         new_task = new_task if new_task else None
         new_priority = int(new_priority) if new_priority else None
 
-        update_task = manager.edit_task(sno , new_task , new_priority)
+        updated_task = manager.edit_task(sno, new_task, new_priority)
 
         print("\nTask updated successfully")
-        print(f"Task :{update_task['task']}")
-        print(f"Priority :{update_task['priority']}")
-    except ValueError as e: 
-        print(f"Error {e}")
+        print(f"Task: {updated_task['task']}")
+        print(f"Priority: {updated_task['priority']}")
+        print(f"Completed: {updated_task['completed']}")
+
+    except ValueError as e:
+        print(f"Error: {e}")
 
 
+def mark_task_complete():
+    try:
+        list_tasks()
+        sno = int(input("\nEnter the serial number of task you want to mark complete: "))
+
+        completed_task = manager.mark_task_complete(sno)
+
+        print("\nTask marked as completed successfully")
+        print(f"Task: {completed_task['task']}")
+        print(f"Priority: {completed_task['priority']}")
+        print(f"Completed: {completed_task['completed']}")
+
+    except ValueError as e:
+        print(f"Error: {e}")
--- task_manager/service.py
+++ task_manager/service.py
@@ -1,72 +1,102 @@
 import uuid
 from pathlib import Path
-from .storage import load_task , save_task
-from .validator import valid_priority , valid_sno ,valid_task
+from .storage import load_task, save_task
+from .validator import valid_priority, valid_sno, valid_task
+
+
 class TaskManager:
     def __init__(self):
-        self.path = Path(__file__).resolve().parent.parent /"data"/"tasks.json" # Ot wo;; later move to config
+        self.path = Path(__file__).resolve().parent.parent / "data" / "tasks.json"
         self.tasks = load_task(self.path)
-    
-    def add_task(self , task , priority): 
-        # genrating task id
+
+    def add_task(self, task, priority):
         task_id = str(uuid.uuid4())
 
-        # Handeling priority
         valid_priority(priority)
-        # checking for valid task
         task = valid_task(task)
-       # append it to the task.json
+
+        completed = False
 
         self.tasks[task_id] = {
-            'task' : task,
-            'priority' : priority
+            'task': task,
+            'priority': priority,
+            'completed': completed
         }
-        save_task(self.path , self.tasks)
 
-        return {"task_id": task_id, "task": task, "priority": priority}
-    
+        save_task(self.path, self.tasks)
 
-    def sort_tasks(self): 
-        sorted_tasks =  sorted(
-            self.tasks.items() , 
-            key = lambda task_item : task_item[1]['priority'])
+        return {
+            "task_id": task_id,
+            "task": task,
+            "priority": priority,
+            "completed": completed
+        }
+
+    def mark_task_complete(self, sno):
+        required_id = self.get_task_id_by_sno(sno)
+        completed_task = self.tasks[required_id]
+
+        if completed_task.get('completed', False):
+            raise ValueError("Task is already completed.")
+
+        completed_task['completed'] = True
+        save_task(self.path, self.tasks)
+
+        return completed_task
+
+    def sort_tasks(self):
+        sorted_tasks = sorted(
+            self.tasks.items(),
+            key=lambda task_item: task_item[1]['priority']
+        )
         return sorted_tasks
 
-
-    def list_tasks(self): 
+    def list_tasks(self):
         sorted_tasks = self.sort_tasks()
         result = []
-        for sno , (task_id , detail) in enumerate(sorted_tasks , start=1):
-            result.append((sno,task_id ,  detail ))
+
+        for sno, (task_id, detail) in enumerate(sorted_tasks, start=1):
+            if 'completed' not in detail:
+                detail['completed'] = False
+            result.append((sno, task_id, detail))
+
         return result
 
-    def get_task_id_by_sno(self , sno): 
+    def get_task_id_by_sno(self, sno):
         sorted_tasks = self.sort_tasks()
-        valid_sno(sorted_tasks , sno)
+        valid_sno(sorted_tasks, sno)
 
-        for serial_no , (task_id , detail) in enumerate(sorted_tasks , start = 1):
-            if serial_no == sno: 
+        for serial_no, (task_id, detail) in enumerate(sorted_tasks, start=1):
+            if serial_no == sno:
                 return task_id
-            
-        raise ValueError('Invalid serial number')
 
-    def delete_task(self , sno): 
+        raise ValueError("Invalid serial number")
+
+    def delete_task(self, sno):
         delete_id = self.get_task_id_by_sno(sno)
-        #  Delete the selected task 
         task_to_delete = self.tasks[delete_id]
+
+        if 'completed' not in task_to_delete:
+            task_to_delete['completed'] = False
+
         del self.tasks[delete_id]
-        save_task(self.path , self.tasks)
+        save_task(self.path, self.tasks)
+
         return task_to_delete
-    
-    def edit_task(self ,sno, new_task = None , new_priority = None):
+
+    def edit_task(self, sno, new_task=None, new_priority=None):
         edit_task_id = self.get_task_id_by_sno(sno)
 
         if new_task is not None:
             new_task = valid_task(new_task)
             self.tasks[edit_task_id]['task'] = new_task
-        if  new_priority is not None: 
+
+        if new_priority is not None:
             valid_priority(new_priority)
             self.tasks[edit_task_id]['priority'] = new_priority
-        save_task(self.path , self.tasks)
-        return self.tasks[edit_task_id]
 
+        if 'completed' not in self.tasks[edit_task_id]:
+            self.tasks[edit_task_id]['completed'] = False
+
+        save_task(self.path, self.tasks)
+        return self.tasks[edit_task_id]
